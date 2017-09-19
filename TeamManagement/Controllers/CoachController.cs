cb5516217965df45242370f3be9010fd927a04ed
vangt <vangt86@gmail.com>
2017-09-19 14:00:10 -0500

    ActionResult Edit to save changes to coach information when changed.

---
+++ TeamManagement/Controllers/CoachController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamManagement.Models;
+
+namespace TeamManagement.Controllers
+{
+    public class CoachController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        
+        // GET: Coach
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var user = User.Identity.GetUserName();
+            var coach = context.Users.Where(x=> x.UserName == user && x.Role == "Coach").First();
+
+            return View(coach);
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            var user = User.Identity.GetUserName();
+            var coach = context.Users.Where(x => x.UserName == user && x.Role == "Coach").First();
+
+            return View(coach);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ApplicationUser coach)
+        {
+            var user = User.Identity.GetUserName();
+            var coachInfo = context.Users.Where(x => x.UserName == user && x.Role == "Coach").First();
+
+            coachInfo.FirstName = coach.FirstName;
+            coachInfo.LastName = coach.LastName;
+            coachInfo.Address = coach.Address;
+            coachInfo.Zip = coach.Zip;
+            coachInfo.PhoneNumber = coach.Phone;
+            coachInfo.Email = coach.Email;
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Coach");
+        }
+    }
+}
