466ec7cbc5f9439c9cd409446cc8d7e37af693cd
vangt <vangt86@gmail.com>
2017-09-21 13:51:13 -0500

    Made the alerts view display user alerts.

--- TeamManagement/Controllers/AlertsController.cs
+++ TeamManagement/Controllers/AlertsController.cs
@@ -1,17 +1,28 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamManagement.Models;
 
 namespace TeamManagement.Controllers
 {
     public class AlertsController : Controller
     {
+        ApplicationDbContext context = new ApplicationDbContext();
+
         // GET: Alerts
         public ActionResult Index()
         {
-            return View();
+            var userName = User.Identity.GetUserName();
+            var user = context.Users.Where(x => x.UserName == userName).First();
+
+            AlertModels alerts = new AlertModels();
+            alerts.Alerts = context.Alerts.Where(x => x.AspNetUsersId == user.Id).ToList();
+            alerts.AspNetUsers = user;
+
+            return View(alerts);
         }
     }
 }
